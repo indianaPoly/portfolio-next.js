d5079c3ba5588ebddf7ee77067858c11d4353e01
george <hyunrim03@naver.com>
2024-03-11 14:42:27 +0900

    feat: navbar 기능 구현

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,135 +1,88 @@
-import { LanguageContext } from "@/contexts/languageContext";
+"use client";
+
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { NavbarItems } from "./navbarItems";
 import Link from "next/link";
-import { useContext, useEffect, useState } from "react";
+import { NavbarItem } from "@/types";
 
-interface HomeLogo {
-  username: string;
-}
+export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-interface LanguageContextProps {
-  language: string;
-  toggleLanguage: () => void;
-}
-
-const Navbar = ({ scrolls }: { scrolls: boolean }) => {
-  const { language, toggleLanguage } = useContext(LanguageContext);
-  const navMenu = [
+  const navbarItem: NavbarItem[] = [
     {
-      projectKor: "프로젝트",
-      projectEng: "PROJECT",
-      link: "project",
-    },
-    {
-      projectKor: "활동 내역",
-      projectEng: "ACTIVITY",
+      name: "활동 내역",
       link: "activity",
     },
     {
-      projectKor: "연락",
-      projectEng: "CONTACT",
-      link: "contact",
+      name: "프로젝트",
+      link: "project",
+    },
+    {
+      name: "블로그",
+      link: "blogs",
     },
   ];
 
+  const NAVBAR_VARIANTS = {
+    visible: {
+      opacity: 1,
+      transition: {
+        x: { velocity: 100 },
+        duration: 0.2,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      transition: {
+        x: { velocity: 100 },
+        duration: 0.2,
+      },
+    },
+  };
+
+  const USER_NAME = "Poly";
+
   return (
-    <nav
-      className={`w-full p-10 flex justify-between items-center ${
-        scrolls ? " hidden" : ""
-      }`}
-    >
-      {/* 로고 및 포트폴리오 이름 */}
-      {language === "kor" ? (
-        <HomeLogo username="Poly's 이야기" />
-      ) : (
-        <HomeLogo username="Poly's Story" />
-      )}
-      {/* 페이지 이동 링크 */}
-      <div className="flex flex-row ">
-        <ul className=" list-none flex text-white font-semibold">
-          {language === "kor"
-            ? navMenu.map((menuData) => {
-                return (
-                  <Menu
-                    menuName={menuData.projectKor}
-                    linkName={menuData.link}
-                    key={menuData.link}
-                  />
-                );
-              })
-            : navMenu.map((menuData) => {
-                return (
-                  <Menu
-                    menuName={menuData.projectEng}
-                    linkName={menuData.link}
-                    key={menuData.link}
-                  />
-                );
-              })}
-        </ul>
-        {/* 언어 변경 */}
-        <LanguageChangeButton
-          language={language}
-          toggleLanguage={toggleLanguage}
-        />
-      </div>
-    </nav>
+    <>
+      <nav className="flex flex-col">
+        <div className="flex items-center justify-between p-[20px]">
+          <Link href="/">
+            <span className=" text-black font-semibold text-[30px]">
+              {USER_NAME}의 대학생활
+            </span>
+          </Link>
+          <ul className="flex gap-[30px]">
+            {navbarItem.map((item) => {
+              return (
+                <li key={item.link}>
+                  <Link href={item.link}>
+                    <span className="text-gray2 font-medium text-[15px] hover:text-black">
+                      {item.name}
+                    </span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+          {/* <button className="" onClick={() => setIsOpen(!isOpen)}>=</button> */}
+        </div>
+
+        {/* <div>
+          <AnimatePresence>
+            {isOpen && (
+              <motion.div
+                initial="hidden"
+                animate={isOpen ? "visible" : "hidden"}
+                exit="hidden"
+                variants={NAVBAR_VARIANTS}
+              >
+                <NavbarItems navbarItem={navbarItem} />
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div> */}
+      </nav>
+    </>
   );
 };
-
-const LanguageChangeButton = ({
-  language,
-  toggleLanguage,
-}: LanguageContextProps) => {
-  return (
-    <button
-      className=" text-white font-semibold"
-      type="button"
-      onClick={toggleLanguage}
-    >
-      {language === "kor" ? (
-        <span>Change Engilsh</span>
-      ) : (
-        <span>한국어로 변경하기</span>
-      )}
-    </button>
-  );
-};
-
-const HomeLogo = ({ username }: HomeLogo) => {
-  return (
-    <Link href="/">
-      <figure></figure>
-      <figcaption className="text-[25px] text-white font-semibold">
-        {username}
-      </figcaption>
-    </Link>
-  );
-};
-
-interface MenuProp {
-  linkName: string;
-  menuName: string;
-}
-
-const Menu = ({ linkName, menuName }: MenuProp) => {
-  const [path, setPath] = useState<string>();
-
-  useEffect(() => {
-    const current = window.location.href;
-    const url = new URL(current);
-    const path = url.pathname.slice(1);
-    setPath(path);
-  }, []);
-  return (
-    <Link href={linkName}>
-      <div className="flex flex-col items-center pr-6">
-        <li>{menuName}</li>
-        {path && path === linkName ? (
-          <div className=" top-[65%] absolute w-[6px] h-[6px] rounded-[50%] bg-[#F5DCB1] animate-ping opacity-100" />
-        ) : null}
-      </div>
-    </Link>
-  );
-};
-
-export default Navbar;
