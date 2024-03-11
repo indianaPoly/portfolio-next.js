5190757db52825bfc7be9adf90d64771f782e0a2
george <hyunrim03@naver.com>
2024-03-11 14:40:14 +0900

    design: color 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      textColor: {
+        white: "#FFFFFF",
+        black: "#111111",
+        gray1: "#525252",
+        gray2: "#d4d7db",
+        gray3: "#ececec",
+        gray4: "#f9fafb",
+        bluegray1: "#98a4cd",
+        bluegray2: "#d8def2",
+        bluegray3: "#e9ecf6",
+        bluegray4: "#f4f6fc",
       },
     },
   },
