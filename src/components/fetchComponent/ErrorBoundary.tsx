0636b75d963a70e926f3dfd181434b86d9a0efb6
george <hyunrim03@naver.com>
2024-03-14 15:36:09 +0900

    feat: fetching 관련 컴포넌트 제작

---
+++ src/components/fetchComponent/ErrorBoundary.tsx
@@ -0,0 +1,30 @@
+import React, { ReactNode } from "react";
+
+interface Props {
+  fallback: ReactNode;
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: any) {
+    console.log(error);
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
---
+++ src/components/fetchComponent/Loading.tsx
@@ -0,0 +1,3 @@
+export const Loading = () => {
+  return <div>데이터 로딩 중입니다.</div>;
+};
